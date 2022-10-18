d7ce0a64bfa396c3255f6e4ae1faa04669ab8387
ShihYLin <lin1634@purdue.edu>
2022-10-18 17:14:18 -0400

    add json file

--- Lab4/acre/acrelog/models.py
+++ Lab4/acre/acrelog/models.py
@@ -4,17 +4,17 @@ blank=True
 
 # Create your models here.
 class Field(models.Model):
-    field_name = models.CharField(max_length=50)
+    field_name = models.CharField(max_length=50, default='field_name')
     soil_type = models.CharField(max_length=50)
-    location = models.CharField(max_length=50)
+    location = models.CharField(max_length=50, default='location')
 
     def __str__(self):
-        return f"Field number: {self.field_num}"
+        return f"Field name: {self.field_name}"
 
 class Crop(models.Model):
     crop_name = models.CharField(max_length=50)
-    scientific_name = models.CharField(max_length=50)
-    life_form = models.CharField(max_length=50)
+    scientific_name = models.CharField(max_length=50, default='sci_name')
+    life_form = models.CharField(max_length=50, default='form')
 
     def __str__(self):
         return f"Crop name: {self.crop_name}"
@@ -22,7 +22,7 @@ class Crop(models.Model):
 class Chemical(models.Model):
     chemical_type = models.CharField(max_length=50)
     date_applied = models.DateTimeField()
-    chemical_name = models.CharField(max_length=50)
+    chemical_name = models.CharField(max_length=50, default='chemical')
 
     def __str__(self):
         return f"{self.chemical_type} applied on {self.date_applied}"
