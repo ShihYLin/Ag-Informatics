496af897951580274fdbb20a3976ad070139cb3b
ShihYLin <lin1634@purdue.edu>
2022-10-19 16:48:10 -0400

    debug admin page

--- Lab4/acre/acrelog/models.py
+++ Lab4/acre/acrelog/models.py
@@ -15,6 +15,7 @@ class Crop(models.Model):
     crop_name = models.CharField(max_length=50)
     scientific_name = models.CharField(max_length=50, default='sci_name')
     life_form = models.CharField(max_length=50, default='form')
+    field_id = models.ForeignKey("Field", on_delete = models.CASCADE, default='field')
 
     def __str__(self):
         return f"Crop name: {self.crop_name}"
@@ -23,6 +24,8 @@ class Chemical(models.Model):
     chemical_type = models.CharField(max_length=50)
     date_applied = models.DateField()
     chemical_name = models.CharField(max_length=50, default='chemical')
+    chem_field_id = models.ForeignKey("Field", on_delete = models.CASCADE, default='field')
+
 
     def __str__(self):
         return f"{self.chemical_type} applied on {self.date_applied}"
